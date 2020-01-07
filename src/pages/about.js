deb97e73c4cb492042cd344b77b565e2fe00b974
davehallonline <davehall98@gmail.com>
2020-01-07 00:36:03 +0000

    Added page layout, typography, top links and new pages

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+  <div>
+    <h1>About me</h1>
+    <p>I'm a web Application Developer from Canterbury, UK. After graduating from The University of Hertfordshire with a degree in Computer Science, I've worked in most IT roles from low level PC building through to being an application developer.
+
+Outside of work I'll normally be involved in family activities with my wife and boys or else you'll probably find me watching one of the many motorsport series I follow.
+
+I'm a West Ham fan (don't hold it against me) and I'm part of the RaiderNation following the Raiders in the NFL</p>
+  </div>
+  </Layout>  
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <h1>Hi! I'm Dave Hall and I'm building this Gatsby site as part of 100DaysOfGatsby</h1>
+    <p>
+      Lots of updates to come but just the basics right now.
+    </p>
+  </Layout>
+)
